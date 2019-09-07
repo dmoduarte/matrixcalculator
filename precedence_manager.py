c5dbfd98b2ec9c85cfff313d730facd9cb86ce88
David Duarte <david.duarte@xpand-it.com>
2019-09-07 20:45:33 +0100

    started precedence manager

--- precedence_manager.py
+++ precedence_manager.py
@@ -1,11 +1,49 @@
 from enum import Enum, auto
+from util import linked_list
 
 class Operations(Enum):
-	KMULT = auto()
-	MULT = auto()
-	ADD = auto()
-	SUB = auto()
+	KMULT = 4
+	MULT = 3
+	ADD = 2
+	SUB = 1
 
+class OperationRequest:
+	def __init__(self, operator, matrix1, matrix2):
+		self.operator = operator
+		self.matrix1 = matrix1
+		self.matrix2 = matrix2
+
+	def setPrevious(previousOperation):
+		self.prevOp = previousOperation
+
+	def setNext(nextOperation):
+		self.nextOp = nextOperation	
+		
 def consumeRequest(request):
+   operationChain = buildOperationChain(request)
 
-	
+def buildOperationChain(request):
+	head = None
+
+	previousRequest = None
+	for index, item in enumerate(request):
+		if isOperator(item):
+			continue
+
+		matrix1 = request[index]
+		operator = request[index+1]
+		matrix2 = request[index+2]
+
+		opRequest = OperationRequest(operator, matrix1, matrix2)
+
+		if head is None:
+			head = opRequest
+
+		if previousRequest is not None:
+			opRequest.setPrevious(previousRequest)
+			previousRequest.setNext(opRequest)	
+
+	return head
+
+def isOperator(item): 
+	return "op" in item
--- util/linked_list.py
+++ util/linked_list.py
@@ -3,6 +3,15 @@ class LinkedList:
 		self.size = 0
 		self.head = self.tail = None
 
+	@staticmethod
+	def initFromList(list):
+		ll = LinkedList()
+
+		for item in list:
+			ll.addBack(item)
+
+		return ll
+
 	def addFront(item):
 		if(self.head is None and self.tail is None):
 			initLinkedList(item)
@@ -10,7 +19,7 @@ class LinkedList:
 
 		self.head.prevNode = Node(None, self.head, item)
 		self.head = self.head.prevNode	
-		self.size = self.size + 1
+		self.size += 1
 
 	def addBack(item):
 		if(self.head is None and self.tail is None):
@@ -19,10 +28,11 @@ class LinkedList:
 
 		self.tail.next = Node(self.tail, None, item)
 		self.tail = self.tail.next
+		self.size += 1
 
 	def initLinkedList(item):
 		self.head = self.tail = Node(None, None, item)
-		self.size = self.size + 1;	
+		self.size += 1;
 
 class Node:
 	def __init__(self, prevNode, nextNode, item):
